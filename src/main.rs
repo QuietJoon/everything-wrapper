a80514d454ed2a02db0a4f78a383f9ec52e50bb3
YongJoon Joe <developer@yongjoon.net>
2025-02-15 21:02:20 +0900

    Add file extension in result

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use everything_rs::{Everything, EverythingRequestFlags};
-use everything_wrapper::util::{human_readable_size, human_readable_date};
+use everything_wrapper::util::{human_readable_date, human_readable_size};
 use everything_wrapper::wrap::{search, show_results, EverythingSort};
-
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let everything = Everything::new();
@@ -12,19 +11,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     everything.set_request_flags(
         EverythingRequestFlags::FullPathAndFileName
-        | EverythingRequestFlags::Size
-        | EverythingRequestFlags::DateCreated
-        | EverythingRequestFlags::DateModified,
+            | EverythingRequestFlags::Size
+            | EverythingRequestFlags::DateCreated
+            | EverythingRequestFlags::DateModified,
     );
 
     everything.set_sort(EverythingSort::PathAscending);
     everything.query()?;
 
-    println!("Results: |{:<20}||{:>20}|",1,2);
+    println!("Results: |{:<20}||{:>20}|", 1, 2);
     // Iterate over all found results.
     for (i, path) in everything.full_path_iter().flatten().enumerate() {
-        let directory = std::path::Path::new(&path).parent().unwrap().to_str().unwrap();
-        let file_name = std::path::Path::new(&path).file_name().unwrap().to_str().unwrap();
+        let directory = std::path::Path::new(&path)
+            .parent()
+            .unwrap()
+            .to_str()
+            .unwrap();
+        let file_name = std::path::Path::new(&path)
+            .file_name()
+            .unwrap()
+            .to_str()
+            .unwrap();
         // Check whether the path is a directory or a file.
         let file_type = if std::path::Path::new(&path).is_dir() {
             "D"
@@ -36,18 +43,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let date_created = everything.get_result_created_date(i as u32)?;
         let date_modified = everything.get_result_count_modified_date(i as u32)?;
         println!(
-            "{:>4}: {} {:<100} ({:<80}, {:<20}) {:>20} {} (Created: {})", 
-            i, 
+            "{:>4}: {} {:<100} ({:<80}, {:<20}) {:>20} {} (Created: {})",
+            i,
             file_type,
-            path, 
-            directory, 
-            file_name, 
-            human_readable_size(size), 
-            human_readable_date(date_modified), 
+            path,
+            directory,
+            file_name,
+            human_readable_size(size),
+            human_readable_date(date_modified),
             human_readable_date(date_created)
         );
     }
-
 
     let result = search("system32 folder:", EverythingSort::PathAscending);
     show_results(result.unwrap());
--- src/wrap.rs
+++ src/wrap.rs
@@ -1,11 +1,11 @@
-use everything_rs::{Everything, EverythingRequestFlags};
+use crate::util::{human_readable_date, human_readable_size};
 use derive_more::Display;
-use crate::util::{human_readable_size, human_readable_date};
+use everything_rs::{Everything, EverythingRequestFlags};
 
 // re-export EverythingSort
 pub use everything_rs::EverythingSort;
 
-#[derive(Debug, Display, PartialEq, Eq)]
+#[derive(Clone, Debug, Display, PartialEq, Eq)]
 pub enum FileType {
     Dir,
     File,
@@ -15,6 +15,7 @@ pub struct Item {
     pub full_path: String,
     pub directory: String,
     pub file_name: String,
+    pub file_extension: String,
     pub size: u64,
     pub size_human: String,
     pub date_created: u64,
@@ -23,16 +24,19 @@ pub struct Item {
     pub date_modified_human: String,
 }
 
-pub fn search(query: &str, sort_by: EverythingSort) -> Result<Vec<Item>, Box<dyn std::error::Error>> {
+pub fn search(
+    query: &str,
+    sort_by: EverythingSort,
+) -> Result<Vec<Item>, Box<dyn std::error::Error>> {
     let everything = Everything::new();
 
     everything.set_search(query);
 
     everything.set_request_flags(
         EverythingRequestFlags::FullPathAndFileName
-        | EverythingRequestFlags::Size
-        | EverythingRequestFlags::DateCreated
-        | EverythingRequestFlags::DateModified,
+            | EverythingRequestFlags::Size
+            | EverythingRequestFlags::DateCreated
+            | EverythingRequestFlags::DateModified,
     );
 
     everything.set_sort(sort_by);
@@ -42,21 +46,45 @@ pub fn search(query: &str, sort_by: EverythingSort) -> Result<Vec<Item>, Box<dyn
     let mut items = Vec::with_capacity(item_count as usize);
 
     for (i, path) in everything.full_path_iter().flatten().enumerate() {
-        let an_item = Item {
-            file_type: if std::path::Path::new(&path).is_dir() {
-                FileType::Dir
+        let file_type = if std::path::Path::new(&path).is_dir() {
+            FileType::Dir
+        } else {
+            FileType::File
+        };
+        let o_extension = std::path::Path::new(&path).extension();
+        let extension = if file_type == FileType::Dir {
+            "Folder".to_string()
+        } else {
+            if o_extension.is_none() {
+                "".to_string()
             } else {
-                FileType::File
-            },
+                o_extension.unwrap().to_str().unwrap().to_string()
+            }
+        };
+        let an_item = Item {
+            file_type: file_type.clone(),
             full_path: path.clone(),
-            directory: std::path::Path::new(&path).parent().unwrap().to_str().unwrap().to_string(),
-            file_name: std::path::Path::new(&path).file_name().unwrap().to_str().unwrap().to_string(),
+            directory: std::path::Path::new(&path)
+                .parent()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string(),
+            file_name: std::path::Path::new(&path)
+                .file_name()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_string(),
+            file_extension: extension,
             size: everything.get_result_size(i as u32)?,
             size_human: human_readable_size(everything.get_result_size(i as u32)?),
             date_created: everything.get_result_created_date(i as u32)?,
             date_created_human: human_readable_date(everything.get_result_created_date(i as u32)?),
             date_modified: everything.get_result_count_modified_date(i as u32)?,
-            date_modified_human: human_readable_date(everything.get_result_count_modified_date(i as u32)?),
+            date_modified_human: human_readable_date(
+                everything.get_result_count_modified_date(i as u32)?,
+            ),
         };
         items.push(an_item);
     }
@@ -98,14 +126,15 @@ pub fn search_by_sd(query: &str) -> Result<Vec<Item>, Box<dyn std::error::Error>
 
 // Only for debugging purposes
 pub fn show_results(items: Vec<Item>) {
-    for (i,item) in items.iter().enumerate() {
+    for (i, item) in items.iter().enumerate() {
         println!(
-            "{:>4}: {} {:<100} ({:<80}, {:<20}) {:>20} {} (Created: {})", 
-            i, 
+            "{:>4}: {} {:<100} ({:<80}, {:<20}, {:<8}) {:>20} {} (Created: {})",
+            i,
             item.file_type,
-            item.full_path, 
-            item.directory, 
+            item.full_path,
+            item.directory,
             item.file_name,
+            item.file_extension,
             item.size_human,
             item.date_modified_human,
             item.date_created_human,
